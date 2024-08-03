71d7683ace739bebde0093deb6e41bb014f859fc
maxalok <maxalok.cdr@gmail.com>
2024-08-03 07:56:52 +0530

    utils code discussion i.e. apiError, ApiResponse,asyncHandler

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import connectDB from "./db/index.js";
 dotenv.config({path: './env'})
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is listening at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("mongoDB connection failed !! ", err);
+})
