878899b1c47b4feb7b409a9d71695ed804a6a4a0
maxalok <maxalok.cdr@gmail.com>
2024-08-03 18:27:33 +0530

    setup routes done and server is listening at given  PORT

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
@@ -14,6 +14,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 
 
