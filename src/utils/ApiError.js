3961638a9f5206a23723d99a837d9389198592a8
maxalok <maxalok.cdr@gmail.com>
2024-08-03 11:31:21 +0530

    file handlenig/upload using third party services like clowdnery and multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message = "something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,10 +12,10 @@ class ApiError extends Error{
         this.success = false
         this.errors = errors
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = this.stack
         }else{
-            Error.captureStackTrace(this,this.constructor)
+            Error.captureStackTrace(this, this.constructor)
         }
     }
 }
