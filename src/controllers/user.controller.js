3e5cecf728a799577ddd42bd5eea44bb8c1cb0d5
maxalok <maxalok.cdr@gmail.com>
2024-08-18 15:35:01 +0530

    typo fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -276,7 +276,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return  res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 
@@ -288,7 +288,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
